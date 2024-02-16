80b24306da344151716702a46a5828d3e88acbac
lolonzolo <159142713+lolonzolo@users.noreply.github.com>
2024-02-16 11:44:09 +0100

    Add files via upload

---
+++ DICTIONARY/dizionario_tess_griglia.py
@@ -0,0 +1,328 @@
+dictionary = {
+    "conf_1":
+       {
+    "ds_num": 1,
+
+    "num_celle" : 5,
+    "num_ap" : 170,
+    "num_bt" : 50,
+    "num_buildings" : 1,
+    "num_floor" : [10], 
+
+    "tessellation" : [[1,1,1,1,1,1,1,1,1,1]], #lista, il numero del piano sara l'indice del elemento nella lista che ci interessa
+    "grid_rows" : [[15,15,15,15,15,15,15,15,15]],
+    "grid_cols" : [[15,15,15,15,15,15,15,15,15]],
+    "num_stanze_per_piano" : [[10,10,10,10,10,10,10,10,10,10]],
+    "num_tiles_per_floor" : [[76,76,76,76,76,76,76,76,76,76]], 
+    "densita_collegamenti" : [[10,10,10,10,10,10,10,10,10,10]],
+    
+    "num_buildings_da_associare_al_dataset" : [1],
+    "num_celle_da_associare_al_dataset" : [3], #per ogni dataset
+    "num_ap_da_associare_al_dataset" : [170],
+    "num_bt_da_associare_al_dataset" : [50],
+    "num_devices_da_associare_al_dataset" : [3],
+    "num_user_da_associare_al_dataset" : [3],
+    "num_fingerprints" : [[3000]], #per ogni dataset e building (primo dataset primo building)
+    "num_radiomap" : [[1000]], #per ogni dataset e building
+    "min_cell_num" : [[1]], #per ogni ds e building
+    "max_cell_num" : [[3]], #per ogni ds e building
+    "min_ap_num" : [[1]], #per ogni ds e building
+    "max_ap_num" : [[40]], #per ogni ds e building
+    "min_bt_num" : [[1]], #per ogni ds e building
+    "max_bt_num" : [[50]], #per ogni ds e building
+    "freq_camp" : [[100]],
+    "t_min" : [[1]],
+    "t_max" : [[10]]
+
+},
+"conf_2":
+       {
+    "ds_num": 1,
+
+    "num_celle" : 5,
+    "num_ap" : 170,
+    "num_bt" : 50,
+    "num_buildings" : 1,
+    "num_floor" : [10], 
+
+    "tessellation" :[[1,1,1,1,1,1,1,1,1,1]], #lista, il numero del piano sara l'indice del elemento nella lista che ci interessa
+    "grid_rows" : [[23,23,23,23,23,23,23,23,23,23]],
+    "grid_cols" : [[23,23,23,23,23,23,23,23,23,23]],
+    "num_stanze_per_piano" : [[10,10,10,10,10,10,10,10,10,10]],
+    "num_tiles_per_floor" : [[110,110,110,110,110,110,110,110,110,110]],
+    "densita_collegamenti" : [[10,10,10,10,10,10,10,10,10,10]],
+    
+    "num_buildings_da_associare_al_dataset" : [1],
+    "num_celle_da_associare_al_dataset" : [3], #per ogni dataset
+    "num_ap_da_associare_al_dataset" : [170],
+    "num_bt_da_associare_al_dataset" : [50],
+    "num_devices_da_associare_al_dataset" : [3],
+    "num_user_da_associare_al_dataset" : [3],
+    "num_fingerprints" : [[3000]], #per ogni dataset e building (primo dataset primo building)
+    "num_radiomap" : [[1000]], #per ogni dataset e building
+    "min_cell_num" : [[1]], #per ogni ds e building
+    "max_cell_num" : [[3]], #per ogni ds e building
+    "min_ap_num" : [[1]], #per ogni ds e building
+    "max_ap_num" : [[40]], #per ogni ds e building
+    "min_bt_num" : [[1]], #per ogni ds e building
+    "max_bt_num" : [[50]], #per ogni ds e building
+    "freq_camp" : [[100]],
+    "t_min" : [[1]],
+    "t_max" : [[10]]
+
+},
+
+"conf_3":
+       {
+    "ds_num": 1,
+
+    "num_celle" : 5,
+    "num_ap" : 170,
+    "num_bt" : 50,
+    "num_buildings" : 1,
+    "num_floor" : [10], 
+
+    "tessellation" : [[1,1,1,1,1,1,1,1,1,1]], #lista, il numero del piano sara l'indice del elemento nella lista che ci interessa
+    "grid_rows" : [[40,40,40,40,40,40,40,40,40,40]],
+    "grid_cols" : [[40,40,40,40,40,40,40,40,40,40]],
+    "num_stanze_per_piano" : [[10,10,10,10,10,10,10,10,10,10]],
+    "num_tiles_per_floor" : [[180,180,180,180,180,180,180,180,180,180]],
+    "densita_collegamenti" : [[10,10,10,10,10,10,10,10,10,10]],
+    
+    "num_buildings_da_associare_al_dataset" : [1],
+    "num_celle_da_associare_al_dataset" : [3], #per ogni dataset
+    "num_ap_da_associare_al_dataset" : [170],
+    "num_bt_da_associare_al_dataset" : [50],
+    "num_devices_da_associare_al_dataset" : [3],
+    "num_user_da_associare_al_dataset" : [3],
+    "num_fingerprints" : [[3000]], #per ogni dataset e building (primo dataset primo building)
+    "num_radiomap" : [[1000]], #per ogni dataset e building
+    "min_cell_num" : [[1]], #per ogni ds e building
+    "max_cell_num" : [[3]], #per ogni ds e building
+    "min_ap_num" : [[1]], #per ogni ds e building
+    "max_ap_num" : [[40]], #per ogni ds e building
+    "min_bt_num" : [[1]], #per ogni ds e building
+    "max_bt_num" : [[50]], #per ogni ds e building
+    "freq_camp" : [[100]],
+    "t_min" : [[1]],
+    "t_max" : [[10]]
+
+},
+"conf_4":
+       {
+    "ds_num": 1,
+
+    "num_celle" : 5,
+    "num_ap" : 170,
+    "num_bt" : 50,
+    "num_buildings" : 1,
+    "num_floor" : [10], 
+
+    "tessellation" : [[1,1,1,1,1,1,1,1,1,1]], #lista, il numero del piano sara l'indice del elemento nella lista che ci interessa
+    "grid_rows" : [[42,42,42,42,42,42,42,42,42,42]],
+    "grid_cols" : [[42,42,42,42,42,42,42,42,42,42]],
+    "num_stanze_per_piano" : [[10,10,10,10,10,10,10,10,10,10]],
+    "num_tiles_per_floor" : [[200,200,200,200,200,200,200,200,200,200]],
+    "densita_collegamenti" : [[10,10,10,10,10,10,10,10,10,10]],
+    
+    "num_buildings_da_associare_al_dataset" : [1],
+    "num_celle_da_associare_al_dataset" : [3], #per ogni dataset
+    "num_ap_da_associare_al_dataset" : [170],
+    "num_bt_da_associare_al_dataset" : [50],
+    "num_devices_da_associare_al_dataset" : [3],
+    "num_user_da_associare_al_dataset" : [3],
+    "num_fingerprints" : [[3000]], #per ogni dataset e building (primo dataset primo building)
+    "num_radiomap" : [[1000]], #per ogni dataset e building
+    "min_cell_num" : [[1]], #per ogni ds e building
+    "max_cell_num" : [[3]], #per ogni ds e building
+    "min_ap_num" : [[1]], #per ogni ds e building
+    "max_ap_num" : [[40]], #per ogni ds e building
+    "min_bt_num" : [[1]], #per ogni ds e building
+    "max_bt_num" : [[50]], #per ogni ds e building
+    "freq_camp" : [[100]],
+    "t_min" : [[1]],
+    "t_max" : [[10]]
+
+},
+"conf_5":
+       {
+    "ds_num": 1,
+
+    "num_celle" : 5,
+    "num_ap" : 170,
+    "num_bt" : 50,
+    "num_buildings" : 1,
+    "num_floor" : [10], 
+
+    "tessellation" : [[1,1,1,1,1,1,1,1,1,1]], #lista, il numero del piano sara l'indice del elemento nella lista che ci interessa
+    "grid_rows" : [[44,44,44,44,44,44,44,44,44,44]],
+    "grid_cols" : [[44,44,44,44,44,44,44,44,44,44]],
+    "num_stanze_per_piano" : [[10,10,10,10,10,10,10,10,10,10]],
+    "num_tiles_per_floor" : [[290,290,290,290,290,290,290,290,290,290]],
+    "densita_collegamenti" : [[10,10,10,10,10,10,10,10,10,10]],
+    
+    "num_buildings_da_associare_al_dataset" : [1],
+    "num_celle_da_associare_al_dataset" : [3], #per ogni dataset
+    "num_ap_da_associare_al_dataset" : [170],
+    "num_bt_da_associare_al_dataset" : [50],
+    "num_devices_da_associare_al_dataset" : [3],
+    "num_user_da_associare_al_dataset" : [3],
+    "num_fingerprints" : [[3000]], #per ogni dataset e building (primo dataset primo building)
+    "num_radiomap" : [[1000]], #per ogni dataset e building
+    "min_cell_num" : [[1]], #per ogni ds e building
+    "max_cell_num" : [[3]], #per ogni ds e building
+    "min_ap_num" : [[1]], #per ogni ds e building
+    "max_ap_num" : [[40]], #per ogni ds e building
+    "min_bt_num" : [[1]], #per ogni ds e building
+    "max_bt_num" : [[50]], #per ogni ds e building
+    "freq_camp" : [[100]],
+    "t_min" : [[1]],
+    "t_max" : [[10]]
+
+},
+"conf_6":
+       {
+    "ds_num": 1,
+
+    "num_celle" : 5,
+    "num_ap" : 170,
+    "num_bt" : 50,
+    "num_buildings" : 1,
+    "num_floor" : [10], 
+
+    "tessellation" : [[1,1,1,1,1,1,1,1,1,1]], #lista, il numero del piano sara l'indice del elemento nella lista che ci interessa
+    "grid_rows" : [[55,55,55,55,55,55,55,55,55,55]],
+    "grid_cols" : [[55,55,55,55,55,55,55,55,55,55]],
+    "num_stanze_per_piano" : [[10,10,10,10,10,10,10,10,10,10]],
+    "num_tiles_per_floor" : [[320,320,320,320,320,320,320,320,320,320]],
+    "densita_collegamenti" : [[10,10,10,10,10,10,10,10,10,10]],
+    
+    "num_buildings_da_associare_al_dataset" : [1],
+    "num_celle_da_associare_al_dataset" : [3], #per ogni dataset
+    "num_ap_da_associare_al_dataset" : [170],
+    "num_bt_da_associare_al_dataset" : [50],
+    "num_devices_da_associare_al_dataset" : [3],
+    "num_user_da_associare_al_dataset" : [3],
+    "num_fingerprints" : [[3000]], #per ogni dataset e building (primo dataset primo building)
+    "num_radiomap" : [[1000]], #per ogni dataset e building
+    "min_cell_num" : [[1]], #per ogni ds e building
+    "max_cell_num" : [[3]], #per ogni ds e building
+    "min_ap_num" : [[1]], #per ogni ds e building
+    "max_ap_num" : [[40]], #per ogni ds e building
+    "min_bt_num" : [[1]], #per ogni ds e building
+    "max_bt_num" : [[50]], #per ogni ds e building
+    "freq_camp" : [[100]],
+    "t_min" : [[1]],
+    "t_max" : [[10]]
+
+},
+"conf_7":
+       {
+    "ds_num": 1,
+
+    "num_celle" : 5,
+    "num_ap" : 170,
+    "num_bt" : 50,
+    "num_buildings" : 1,
+    "num_floor" : [10], 
+
+    "tessellation" : [[1,1,1,1,1,1,1,1,1,1]], #lista, il numero del piano sara l'indice del elemento nella lista che ci interessa
+    "grid_rows" : [[63,63,63,63,63,63,63,63,63,63]],
+    "grid_cols" : [[63,63,63,63,63,63,63,63,63,63]],
+    "num_stanze_per_piano" : [[10,10,10,10,10,10,10,10,10,10]],
+    "num_tiles_per_floor" : [[380,380,380,380,380,380,380,380,380,380]],
+    "densita_collegamenti" : [[10,10,10,10,10,10,10,10,10,10]],
+    
+    "num_buildings_da_associare_al_dataset" : [1],
+    "num_celle_da_associare_al_dataset" : [3], #per ogni dataset
+    "num_ap_da_associare_al_dataset" : [170],
+    "num_bt_da_associare_al_dataset" : [50],
+    "num_devices_da_associare_al_dataset" : [3],
+    "num_user_da_associare_al_dataset" : [3],
+    "num_fingerprints" : [[3000]], #per ogni dataset e building (primo dataset primo building)
+    "num_radiomap" : [[1000]], #per ogni dataset e building
+    "min_cell_num" : [[1]], #per ogni ds e building
+    "max_cell_num" : [[3]], #per ogni ds e building
+    "min_ap_num" : [[1]], #per ogni ds e building
+    "max_ap_num" : [[40]], #per ogni ds e building
+    "min_bt_num" : [[1]], #per ogni ds e building
+    "max_bt_num" : [[50]], #per ogni ds e building
+    "freq_camp" : [[100]],
+    "t_min" : [[1]],
+    "t_max" : [[10]]
+
+},
+"conf_8":
+       {
+    "ds_num": 1,
+
+    "num_celle" : 5,
+    "num_ap" : 170,
+    "num_bt" : 50,
+    "num_buildings" : 1,
+    "num_floor" : [10], 
+
+    "tessellation" : [[1,1,1,1,1,1,1,1,1,1]], #lista, il numero del piano sara l'indice del elemento nella lista che ci interessa
+    "grid_rows" : [[67,67,67,67,67,67,67,67,67,67]],
+    "grid_cols" : [[67,67,67,67,67,67,67,67,67,67]],
+    "num_stanze_per_piano" : [[10,10,10,10,10,10,10,10,10,10]],
+    "num_tiles_per_floor" : [[460,460,460,460,460,460,460,460,460,460]],
+    "densita_collegamenti" : [[10,10,10,10,10,10,10,10,10,10]],
+    
+    "num_buildings_da_associare_al_dataset" : [1],
+    "num_celle_da_associare_al_dataset" : [3], #per ogni dataset
+    "num_ap_da_associare_al_dataset" : [170],
+    "num_bt_da_associare_al_dataset" : [50],
+    "num_devices_da_associare_al_dataset" : [3],
+    "num_user_da_associare_al_dataset" : [3],
+    "num_fingerprints" : [[3000]], #per ogni dataset e building (primo dataset primo building)
+    "num_radiomap" : [[1000]], #per ogni dataset e building
+    "min_cell_num" : [[1]], #per ogni ds e building
+    "max_cell_num" : [[3]], #per ogni ds e building
+    "min_ap_num" : [[1]], #per ogni ds e building
+    "max_ap_num" : [[40]], #per ogni ds e building
+    "min_bt_num" : [[1]], #per ogni ds e building
+    "max_bt_num" : [[50]], #per ogni ds e building
+    "freq_camp" : [[100]],
+    "t_min" : [[1]],
+    "t_max" : [[10]]
+
+},
+"conf_9":
+       {
+    "ds_num": 1,
+
+    "num_celle" : 5,
+    "num_ap" : 170,
+    "num_bt" : 50,
+    "num_buildings" : 1,
+    "num_floor" : [10], 
+
+    "tessellation" : [[1,1,1,1,1,1,1,1,1,1]], #lista, il numero del piano sara l'indice del elemento nella lista che ci interessa
+    "grid_rows" : [[80,80,80,80,80,80,80,80,80,80]],
+    "grid_rows" : [[80,80,80,80,80,80,80,80,80,80]],
+    "num_stanze_per_piano" : [[10,10,10,10,10,10,10,10,10,10]],
+    "num_tiles_per_floor" : [[950,950,950,950,950,950,950,950,950,950]],
+    "densita_collegamenti" : [[10,10,10,10,10,10,10,10,10,10]],
+    
+    "num_buildings_da_associare_al_dataset" : [1],
+    "num_celle_da_associare_al_dataset" : [3], #per ogni dataset
+    "num_ap_da_associare_al_dataset" : [170],
+    "num_bt_da_associare_al_dataset" : [50],
+    "num_devices_da_associare_al_dataset" : [3],
+    "num_user_da_associare_al_dataset" : [3],
+    "num_fingerprints" : [[3000]], #per ogni dataset e building (primo dataset primo building)
+    "num_radiomap" : [[100]], #per ogni dataset e building
+    "min_cell_num" : [[1]], #per ogni ds e building
+    "max_cell_num" : [[3]], #per ogni ds e building
+    "min_ap_num" : [[1]], #per ogni ds e building
+    "max_ap_num" : [[40]], #per ogni ds e building
+    "min_bt_num" : [[1]], #per ogni ds e building
+    "max_bt_num" : [[50]], #per ogni ds e building
+    "freq_camp" : [[100]],
+    "t_min" : [[1]],
+    "t_max" : [[10]]
+
+}
+ 
+}
