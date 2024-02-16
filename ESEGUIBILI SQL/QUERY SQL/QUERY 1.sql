3354b31c43063540ca6ce371ce86924bd4eb006c
lolonzolo <159142713+lolonzolo@users.noreply.github.com>
2024-02-16 11:48:00 +0100

    Add files via upload

---
+++ ESEGUIBILI SQL/QUERY SQL/QUERY 1.sql
@@ -0,0 +1,6 @@
+
+explain analyze
+select fingerprint.id
+from fingerprint
+where ml_purpose='training';
+
---
+++ ESEGUIBILI SQL/QUERY SQL/QUERY 2.sql
@@ -0,0 +1,10 @@
+
+explain analyze
+select tile.place_id, tile.type
+from tile
+	join tessellation on tile.tessellation_id=tessellation.id 
+	join floor on floor.place_id=tessellation.floor_place_id
+	join contains on floor.place_id=contains.contained_place_id
+where container_place_id=(SELECT building.place_id FROM building ORDER BY RANDOM() LIMIT 1) and not exists (select *
+											from fingerprint 
+											where is_radio_map=true and acquired_at_tile_place_id=tile.place_id)
